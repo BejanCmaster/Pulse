cbcacd098a0b586014f810d7583856a46e3daaf3
Cristian <clickteh@gmail.com>
2022-10-11 16:29:23 +0300

    Section catalog

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -4,7 +4,11 @@ import Swiper, {
 } from 'swiper';
 Swiper.use([Navigation]);
 
-const swiper = new Swiper('.carousel__swiper', {
+const swiper = new Swiper('.carousel-swiper', {
   slidesPerView: "auto",
   loop: true,
+  navigation: {
+    nextEl: ".carousel-button__next",
+    prevEl: ".carousel-button__prev",
+  }
 });
